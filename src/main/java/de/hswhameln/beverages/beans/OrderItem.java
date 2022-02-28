e2e36ba9d407dae53292071239b3be1be4cd5c7b
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-28 10:47:56 +0100

    add order beans

---
+++ src/main/java/de/hswhameln/beverages/beans/OrderItem.java
@@ -0,0 +1,33 @@
+package de.hswhameln.beverages.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    private BigDecimal vat;
+
+    @ManyToOne
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Product product;
+
+
+}
