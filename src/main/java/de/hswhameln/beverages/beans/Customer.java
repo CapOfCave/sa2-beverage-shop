640be47742a45baea2776c03b62a2a4226a13ff8
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-28 10:21:16 +0100

    create bean getters and constructors

--- src/main/java/de/hswhameln/beverages/beans/Customer.java
+++ src/main/java/de/hswhameln/beverages/beans/Customer.java
@@ -34,4 +34,71 @@ public class Customer {
     private String iban;
 
     private boolean active;
+
+    public Customer() {
+    }
+
+    public Customer(String prename, String surname, String password, LocalDate birthdate, String street, String streetno, String zipcode, String city, String country, String iban, boolean active) {
+        this.prename = prename;
+        this.surname = surname;
+        this.password = password;
+        this.birthdate = birthdate;
+        this.street = street;
+        this.streetno = streetno;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.country = country;
+        this.iban = iban;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPrename() {
+        return prename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getStreetno() {
+        return streetno;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+
 }
--- src/main/java/de/hswhameln/beverages/beans/Product.java
+++ src/main/java/de/hswhameln/beverages/beans/Product.java
@@ -18,4 +18,38 @@ public class Product {
     private BigDecimal price;
     private boolean active;
 
+    public Product() {
+    }
+
+    public Product(String shortDescription, String longDescription, String productImage, BigDecimal price, boolean active) {
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.productImage = productImage;
+        this.price = price;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
 }
