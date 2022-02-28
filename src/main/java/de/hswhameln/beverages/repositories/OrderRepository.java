7f6273d512f4408989b45d0790d3479bea091d98
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-28 10:50:03 +0100

    create missing repositories

--- src/main/java/de/hswhameln/beverages/repositories/OrderRepository.java
+++ src/main/java/de/hswhameln/beverages/repositories/OrderRepository.java
@@ -1,10 +1,9 @@
 package de.hswhameln.beverages.repositories;
 
-import de.hswhameln.beverages.beans.Product;
+import de.hswhameln.beverages.beans.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
