92a072bbae01339de6af475804056a067a033f66
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-28 10:16:05 +0100

    create beans

--- src/main/java/de/hswhameln/beverages/beans/Customer.java
+++ src/main/java/de/hswhameln/beverages/beans/Customer.java
@@ -1,2 +1,37 @@
-package de.hswhameln.beverages.beans;public class Customer {
+package de.hswhameln.beverages.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String prename;
+
+    private String surname;
+
+    private String password;
+
+    private LocalDate birthdate;
+
+    private String street;
+
+    private String streetno;
+
+    private String zipcode;
+
+    private String city;
+
+    private String country;
+
+    private String iban;
+
+    private boolean active;
 }
--- src/main/java/de/hswhameln/beverages/beans/Product.java
+++ src/main/java/de/hswhameln/beverages/beans/Product.java
@@ -1,2 +1,21 @@
-package de.hswhameln.beverages.beans;public class Product {
+package de.hswhameln.beverages.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String shortDescription;
+    private String longDescription;
+    private String productImage;
+    private BigDecimal price;
+    private boolean active;
+
 }
